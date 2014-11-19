78891216876b59a9c40d276afa772d3978b5a61f
Andrew Chen <yongjhih@gmail.com>
2014-11-19 12:52:56 +0800

    Rename to SimpleOnLoginListener

---
+++ Simple Facebook/src/com/sromku/simple/fb/listeners/SimpleOnLoginListener.java
@@ -0,0 +1,37 @@
+package com.sromku.simple.fb.listeners;
+
+import android.app.Activity;
+
+import com.sromku.simple.fb.Permission;
+import com.sromku.simple.fb.SimpleFacebook;
+
+/**
+ * On login/logout actions listener
+ * 
+ * @author sromku
+ */
+public class SimpleOnLoginListener implements OnLoginListener {
+    @Override
+    public void onLogin() {
+    }
+
+    @Override
+    public void onLogin(SimpleFacebook simpleFacebook) {
+    }
+
+    @Override
+    public void onNotAcceptingPermissions(Permission.Type type) {
+    }
+
+    @Override
+    public void onException(Throwable throwable) {
+    }
+
+    @Override
+    public void onFail(String reason) {
+    }
+
+    @Override
+    public void onThinking() {
+    }
+}
